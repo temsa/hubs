5b4a621fee7494f1c9792b1127eb963d11956f45
Marshall Polaris <marshall@pol.rs>
2019-04-03 17:07:29 -0700

    Moderately improve cursor target acquisition

--- src/systems/cursor-targetting-system.js
+++ src/systems/cursor-targetting-system.js
@@ -1,14 +1,16 @@
 export class CursorTargettingSystem {
   constructor() {
     this.targets = [];
-    this.setDirty = this.setDirty.bind(this);
     this.dirty = true;
+    this.onMutation = this.onMutation.bind(this);
+    this.onObject3DSet = this.onObject3DSet.bind(this);
+    this.onObject3DRemove = this.onObject3DRemove.bind(this);
     // TODO: Use the MutationRecords passed into the callback function to determine added/removed nodes!
-    this.observer = new MutationObserver(this.setDirty);
     const scene = document.querySelector("a-scene");
+    this.observer = new MutationObserver(this.onMutation);
     this.observer.observe(scene, { childList: true, attributes: true, subtree: true });
-    scene.addEventListener("object3dset", this.setDirty);
-    scene.addEventListener("object3dremove", this.setDirty);
+    scene.addEventListener("object3dset", this.onObject3DSet);
+    scene.addEventListener("object3dremove", this.onObject3DRemove);
     this.rightRemote = document.querySelector("#cursor-controller");
   }
 
@@ -25,6 +27,21 @@ export class CursorTargettingSystem {
     this.rightRemote.components["cursor-controller"].tick2(t);
   }
 
+  onObject3DSet() {
+    this.dirty = true;
+  }
+
+  onObject3DRemove() {
+    this.dirty = true;
+  }
+
+  onMutation(records) {
+    // let's try to avoid re-querying the targets on attribute changes we know we can ignore
+    if (records.some(r => r.type === "childList" || (r.type === "attributes" && r.attributeName === "class"))) {
+      this.dirty = true;
+    }
+  }
+
   populateEntities(targets) {
     targets.length = 0;
     // TODO: Do not querySelectorAll on the entire scene every time anything changes!
@@ -38,7 +55,7 @@ export class CursorTargettingSystem {
 
   remove() {
     this.observer.disconnect();
-    AFRAME.scenes[0].removeEventListener("object3dset", this.setDirty);
-    AFRAME.scenes[0].removeEventListener("object3dremove", this.setDirty);
+    AFRAME.scenes[0].removeEventListener("object3dset", this.onObject3DSet);
+    AFRAME.scenes[0].removeEventListener("object3dremove", this.onObject3DRemove);
   }
 }
